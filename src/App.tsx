4607735e220aadf017ce590104a2d19f7071a9ee
Alex Cantarero Fuentes <alex.cantarero@estudiantat.upc.edu>
2026-04-24 23:51:02 +0200

    /update: set initial 3D scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { CameraControls } from "@react-three/drei";
+import { useRef, useEffect } from "react";
+import * as THREE from "three";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const lightRef = useRef<THREE.DirectionalLight>(null);
+  const meshRef = useRef<THREE.Mesh>(null);
+
+  useEffect(() => {
+    if (lightRef.current && meshRef.current) {
+      lightRef.current.target.position.copy(meshRef.current.position);
+      lightRef.current.target.updateMatrixWorld();
+    }
+  }, []);
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    <div className="canvas-container">
+      <Canvas className="canvas" camera={{ position: [0, 5, 40], fov: 50 }}>
+        <color attach="background" args={["#bbbbbb"]} />
+        <directionalLight ref={lightRef} position={[0, 5, 5]} intensity={5} />
+        {/* <pointLight position={[0, -1.5, 0]} intensity={50} decay={0} /> */}
+        <ambientLight intensity={3} />
+        <mesh position={[0, -2, 0]} ref={meshRef}>
+          <boxGeometry args={[20, 0.25, 20]} />
+          <meshStandardMaterial color="#a3a3a3" />
+        </mesh>
+        <CameraControls makeDefault />
+      </Canvas>
+    </div>
+  );
 }
-
-export default App
