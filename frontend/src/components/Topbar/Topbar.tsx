e3755d8d85f5475f18a88f086a4c331d77ce3c80
Alex Cantarero Fuentes <alex.cantarero@estudiantat.upc.edu>
2026-04-25 15:59:17 +0200

    /update: bays not working

--- frontend/src/components/Topbar/Topbar.tsx
+++ frontend/src/components/Topbar/Topbar.tsx
@@ -2,13 +2,47 @@ import "./Topbar.css";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
 import ClearAllIcon from "@mui/icons-material/ClearAll";
 import VideocamIcon from "@mui/icons-material/Videocam";
-import { Button } from "@mui/material";
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Menu,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
+import { useState } from "react";
 
 type TopbarProps = {
+  caseNumber: number;
+  onCaseChange: (caseNumber: number) => void;
   onToggleCameraView: () => void;
 };
 
-export default function Topbar({ onToggleCameraView }: TopbarProps) {
+export default function Topbar({
+  caseNumber,
+  onCaseChange,
+  onToggleCameraView,
+}: TopbarProps) {
+  const [casesMenuAnchor, setCasesMenuAnchor] = useState<null | HTMLElement>(
+    null,
+  );
+
+  const isCasesMenuOpen = Boolean(casesMenuAnchor);
+
+  const openCasesMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setCasesMenuAnchor(event.currentTarget);
+  };
+
+  const closeCasesMenu = () => {
+    setCasesMenuAnchor(null);
+  };
+
+  const handleCaseChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onCaseChange(Number(event.target.value));
+    closeCasesMenu();
+  };
+
   return (
     <div className="topbar">
       <div className="leading-tooltip">Mecalux Bay Distributor</div>
@@ -21,17 +55,40 @@ export default function Topbar({ onToggleCameraView }: TopbarProps) {
           variant="contained"
           className="bubble"
           sx={{ backgroundColor: "#288aed" }}
+          onClick={onToggleCameraView}
         >
-          <ClearAllIcon sx={{}} />
+          <VideocamIcon />
         </Button>
         <Button
           variant="contained"
           className="bubble"
           sx={{ backgroundColor: "#288aed" }}
-          onClick={onToggleCameraView}
+          onClick={openCasesMenu}
         >
-          <VideocamIcon />
+          <ClearAllIcon sx={{}} />
         </Button>
+        <Menu
+          anchorEl={casesMenuAnchor}
+          open={isCasesMenuOpen}
+          onClose={closeCasesMenu}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          transformOrigin={{ vertical: "top", horizontal: "right" }}
+        >
+          <FormControl sx={{ px: 2, py: 1 }}>
+            <FormLabel id="case-radio-group-label">Display case</FormLabel>
+            <RadioGroup
+              aria-labelledby="case-radio-group-label"
+              name="case-radio-group"
+              value={String(caseNumber)}
+              onChange={handleCaseChange}
+            >
+              <FormControlLabel value="0" control={<Radio />} label="Case 0" />
+              <FormControlLabel value="1" control={<Radio />} label="Case 1" />
+              <FormControlLabel value="2" control={<Radio />} label="Case 2" />
+              <FormControlLabel value="3" control={<Radio />} label="Case 3" />
+            </RadioGroup>
+          </FormControl>
+        </Menu>
       </div>
     </div>
   );
